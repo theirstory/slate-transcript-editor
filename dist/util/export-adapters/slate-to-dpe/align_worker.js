4e7eb5878623ede6bb76f5535cc7b306c3957fb5
Joseph Shearer <joseph@josephshearer.net>
2020-11-27 23:01:21 -0500

    commit dist files

---
+++ dist/util/export-adapters/slate-to-dpe/align_worker.js
@@ -0,0 +1,5 @@
+import alignDiraizedText from 'align-diarized-text/src/index';
+addEventListener('message', event => {
+  const response = alignDiraizedText(...event.data);
+  postMessage(response);
+});
